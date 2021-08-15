44ce5d066964e210fbfd9926facb021495a4cd60
NA-Siddiky <siddiky.academic@gmail.com>
2021-08-15 19:06:11 +0600

    banking managment complete

--- js/banking.js
+++ js/banking.js
@@ -16,13 +16,41 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // update total balance
     const balanceTotal = document.getElementById('balance-total');
     const previousBalanceTotal = balanceTotal.innerText;
-    console.log(previousBalanceTotal)
     const newBalanceTotal = parseFloat(previousBalanceTotal) + parseFloat(newDepositAmount);
 
-    console.log(newBalanceTotal);
+    balanceTotal.innerText = newBalanceTotal;
+
+    // Clean the input box
+    depositInput.value = '';
+});
+
+// handle withdraw button event
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // console.log("clicked")
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    // console.log(withdrawAmountText);
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    // console.log(newWithdrawAmount);
+
+    // set withdraw total
+    const withdrawTotal = document.getElementById("withdraw-total");
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
     balanceTotal.innerText = newBalanceTotal;
 
 
     // Clean the input box
-    depositInput.value = '';
-})
+    withdrawInput.value = '';
+});
